322ff467fabad142417b97a5c0685ff1f4ee0467
DavidG159 <gaspardavid235@gmail.com>
2025-05-18 22:05:03 -0600

    added functionality to btn1

--- script.js
+++ script.js
@@ -4,7 +4,24 @@ const btn1 = document.querySelector('.button--1');
 const btn2 = document.querySelector('.button--2');
 const btn3 = document.querySelector('.button--3');
 
-console.log(btn1);
+
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function setRandomColor() {
+    document.body.style.backgroundColor = getRandomColor();
+}
+
+
 btn1.addEventListener('click', function () {
-    console.log('button1 has been clicked');
+    setRandomColor();
+    console.log('test');
+    console.log(getRandomColor());
 });
