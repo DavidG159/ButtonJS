d8d57c1ad22da3d0c8f9ab1884007783293c5a23
DavidG159 <gaspardavid235@gmail.com>
2025-06-02 22:14:09 -0600

    added counter display JS

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const btn1 = document.querySelector('.button--1');
 const btn2 = document.querySelector('.button--2');
 const btn3 = document.querySelector('.button--3');
 const clicksfx = new Audio('click.mp3');
+const button2_display1 = document.querySelector('.counter-display');
+const button2_display2 = document.querySelector('.reset-btn');
 
 function getRandomColor() {
     let letters = '0123456789ABCDEF';
@@ -25,3 +27,9 @@ btn1.addEventListener('click', function () {
     clicksfx.play();
 
 });
+
+btn2.addEventListener('click', function () {
+    button2_display1.classList.remove('hidden');
+    button2_display2.classList.remove('hidden');
+
+})
