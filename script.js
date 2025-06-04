35637e6a01359af32a884995cd902072b6f5cd7b
DavidG159 <gaspardavid235@gmail.com>
2025-06-03 21:35:28 -0600

    fixed css for close button2

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const btn3 = document.querySelector('.button--3');
 const clicksfx = new Audio('click.mp3');
 const button2_display1 = document.querySelector('.counter-display');
 const button2_display2 = document.querySelector('.reset-btn');
+const button2_display3 = document.querySelector('.close-btn');
 
 function getRandomColor() {
     let letters = '0123456789ABCDEF';
@@ -31,5 +32,7 @@ btn1.addEventListener('click', function () {
 btn2.addEventListener('click', function () {
     button2_display1.classList.remove('hidden');
     button2_display2.classList.remove('hidden');
+    button2_display3.classList.remove('hidden');
+
 
 })
