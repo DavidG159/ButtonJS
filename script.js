2ab9cdc2ceb0927a0dafab1c3e22111c55bcb0e6
DavidG159 <gaspardavid235@gmail.com>
2025-06-04 22:32:17 -0600

    added sfx to button 2

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const btn1 = document.querySelector('.button--1');
 const btn2 = document.querySelector('.button--2');
 const btn3 = document.querySelector('.button--3');
 const clicksfx = new Audio('click.mp3');
+const clicksfx2 = new Audio('click2.mp3');
 const button2_display1 = document.querySelector('.counter-display');
 const button2_display2 = document.querySelector('.reset-btn');
 const button2_display3 = document.querySelector('.close-btn');
@@ -48,6 +49,7 @@ btn2.addEventListener('click', function () {
     button2_display2.classList.remove('hidden');
     button2_display3.classList.remove('hidden');
     incrementCounter();
+    clicksfx2.play();
 
 });
 
